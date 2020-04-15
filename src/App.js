83744daa4d4b7aef78fbf015fbafde46d67cf67c
Marcelo Garcia <mbgarcia_pa@yahoo.com.br>
2020-04-14 22:26:49 -0300

    Adding login and logout with session control

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { LinkContainer } from 'react-router-bootstrap';
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
+import { AppContext } from "./libs/contextLib";
 
 function App() {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+
   return (
     <div className="App container">
       <Navbar fluid collapseOnSelect>
@@ -17,18 +20,29 @@ function App() {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <LinkContainer to="/signup">
-              <NavItem>Signup</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <NavItem>Login</NavItem>
-            </LinkContainer>
+            {isAuthenticated
+              ? <NavItem onClick={handleLogout}>Logout</NavItem>
+              : <>
+                  <LinkContainer to="/signup">
+                    <NavItem>Signup</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <NavItem>Login</NavItem>
+                  </LinkContainer>
+                </>
+            }
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Routes />
+      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+        <Routes />
+      </AppContext.Provider>
     </div>
   );
+
+  function handleLogout() {
+    userHasAuthenticated(false);
+  }  
 }
 
 export default App;
